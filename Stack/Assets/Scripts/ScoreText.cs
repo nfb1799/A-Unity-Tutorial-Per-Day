280f30b81a56fe5fbd997fb90027bd526963e60d
nfb1799 <nfb1799@rit.edu>
2021-05-24 18:21:49 -0400

    Stack Game

---
+++ Stack/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    private int score;
+    private TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>(); 
+        GameManager.OnCubeSpawned += GameManager_OnCubeSpawned;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnCubeSpawned -= GameManager_OnCubeSpawned;
+    }
+
+    private void GameManager_OnCubeSpawned()
+    {
+        score++;
+        text.text = "Score: " + score;
+    }
+}
