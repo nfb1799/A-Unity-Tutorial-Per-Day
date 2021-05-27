740008e0a8b8ebb9b770287e84aca1c4f0aa258a
nfb1799 <nfb1799@rit.edu>
2021-05-27 19:41:05 -0400

    Flappy Bird

---
+++ Flappy Bird/Assets/Scripts/MovePipes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePipes : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * speed;
+    }
+}
