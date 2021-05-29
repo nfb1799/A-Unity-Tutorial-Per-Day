e4227d27bd418da6197fe994d6a13088c7608272
nfb1799 <nfb1799@rit.edu>
2021-05-28 22:50:27 -0400

    Auto Runner

---
+++ Auto Runner/Assets/BackgroundScroller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    private BoxCollider2D collider;
+
+    private Rigidbody2D rb;
+
+    private float width;
+    public float scrollSpeed = -2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = collider.size.x;
+        collider.enabled = false;
+
+        rb.velocity = new Vector2(scrollSpeed, 0f);
+        ResetObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -width)
+        {
+            Vector3 resetPos = new Vector3(width * 3f, 0f, 0f);
+            transform.position = transform.position + resetPos;
+            ResetObstacle();
+        }
+    }
+
+    void ResetObstacle()
+    {
+        transform.GetChild(0).localPosition = new Vector3(3f, Random.Range(-4f, 4f), 0f);
+    }
+}
---
+++ Auto Runner/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertDirection = Input.GetAxisRaw("Vertical");
+        float horDirection = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horDirection * moveSpeed, vertDirection * moveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+            SceneManager.LoadScene(0);
+    }
+}
