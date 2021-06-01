a1ff6ca61a70ab0b9127dc0b7a7d442ffa50aa71
nfb1799 <nfb1799@rit.edu>
2021-05-31 23:57:57 -0400

    Doodle Jump

---
+++ Doodle Jump/Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.relativeVelocity.y <= 0) // Coming from the top
+        {
+            Rigidbody2D rb = coll.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
+        }
+    }
+}
---
+++ Doodle Jump/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    private float movement = 0f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    //Use instead of Update() when working with physics (Rigidbodies)
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
